62c09f8b6d1212890da81cc78d22b7331a6719dd
Blanco <manuelblanco.avila@gmail.com>
2021-03-22 18:09:57 +0100

    Primer dia

---
+++ P.Angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import {Cliente} from './cliente';
+  export const  CLIENTES: Cliente[] =
+[
+{id:1,nombre:'Manuel',apellido:'Blanco', email:'manuelblanco.avila@gmial.com', createAt: '2021-22-3'},
+{id:2,nombre:'Manuel',apellido:'Blanco', email:'manuelblanco.avila@gmial.com', createAt: '2021-22-3'},
+{id:3,nombre:'Manuel',apellido:'Blanco', email:'manuelblanco.avila@gmial.com', createAt: '2021-22-3'},
+{id:4,nombre:'Manuel',apellido:'Blanco', email:'manuelblanco.avila@gmial.com', createAt: '2021-22-3'},
+{id:5,nombre:'Manuel',apellido:'Blanco', email:'manuelblanco.avila@gmial.com', createAt: '2021-22-3'},
+{id:6,nombre:'Manuel',apellido:'Blanco', email:'manuelblanco.avila@gmial.com', createAt: '2021-22-3'},
+{id:7,nombre:'Manuel',apellido:'Blanco', email:'manuelblanco.avila@gmial.com', createAt: '2021-22-3'},
+{id:8,nombre:'Manuel',apellido:'Blanco', email:'manuelblanco.avila@gmial.com', createAt: '2021-22-3'},
+{id:9,nombre:'Manuel',apellido:'Blanco', email:'manuelblanco.avila@gmial.com', createAt: '2021-22-3'},
+
+];
